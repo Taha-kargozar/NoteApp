25690204a607af6332f5940abd2bd9d2cca0edad
taha <taha1060@yahoo.com>
2025-10-23 18:27:54 +0330

    Add NoteController

---
+++ src/main/java/ir/noteapp/Controller/NoteController.java
@@ -0,0 +1,52 @@
+package ir.noteapp.Controller;
+
+import ir.noteapp.Dto.NoteDto;
+import ir.noteapp.Model.Notes;
+import ir.noteapp.Service.NoteService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/note")
+public class NoteController {
+       private final NoteService noteService;
+
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Integer> save(@RequestBody @Valid NoteDto noteDto) {
+        Notes notes = new Notes();
+        notes.setTitle(noteDto.title());
+        notes.setContent(noteDto.content());
+        noteService.saveNote(notes);
+        return ResponseEntity.ok(notes.getId());
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable int id) {
+        noteService.deleteNote(id);
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping("/all")
+    public ResponseEntity<Page<Notes>> all(Pageable pageable){
+        Page<Notes> notesPage = noteService.getAllNotes(pageable);
+        return ResponseEntity.ok(notesPage);
+    }
+    @PostMapping("/update/{id}")
+    public ResponseEntity<Integer> update(@PathVariable int id ,@RequestBody @Valid NoteDto noteDto) {
+        Notes notes = noteService.getbyId(id);
+        notes.setContent(noteDto.content());
+        notes.setTitle(noteDto.title());
+        noteService.saveNote(notes);
+        return ResponseEntity.ok().body(notes.getId());
+    }
+    @PostMapping("/findbyId/{id}")
+    public ResponseEntity<Notes> findbyId(@PathVariable int id) {
+        Notes notes = noteService.getbyId(id);
+        return ResponseEntity.ok(notes);
+    }
+}
